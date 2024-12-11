5120cf03c4556d6e965ed34cef11d5c12904b729
Hawkeye2108 <jatinrawat2108@gmail.com>
2024-12-11 21:03:06 +0530

    User 2

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -26,7 +26,7 @@ const registerUser = async (req,res)=>{
       // create a jwt token
     //   const token = createToken(user._id);
     //   res.status(200).json({email,token});
-      res.status(200).json({email});
+      res.status(200).json({name:user.name,email,profile,phone:user.phone});
     } catch (error) {
       res.status(400).json({error: error.message})
     }
